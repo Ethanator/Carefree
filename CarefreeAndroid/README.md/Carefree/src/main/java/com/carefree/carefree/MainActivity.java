4abf42d968a433799d8a5f75288cacf7b49f6929
alexmwu <awu3@nd.edu>
2014-04-12 19:16:19 -0400

    User Input
    user inputs basic info

--- CarefreeAndroid/README.md/Carefree/src/main/java/com/carefree/carefree/MainActivity.java
+++ CarefreeAndroid/README.md/Carefree/src/main/java/com/carefree/carefree/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends Activity {
     public void sendMessage(View view) {
         // Do something in response to button
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
+        EditText editText = (EditText) findViewById(R.id.allergies);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
